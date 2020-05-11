838a107ca190ec3a2e90d5e018e36fcd43f85361
imdonnie <donniexero@gmail.com>
2020-05-10 22:23:26 -0500

    complete main.py

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
+import os
 from helper.Tools import *
 from helper.Plotter import *
 from helper.Format import *
 from core.slope import *
-import os
+from core.hiden_markov import *
+from core.point_wise_map import *
+
 if __name__ == "__main__":
 # visualizaiton part of the link data
-    # link_data = load('link data')
-    # plotLinkData(link_data)
-
-    # cleanLinkData()
-    # links = loadCleanData('links')
-    # calculateSlope(links)
-    # evaluateSlope()
-
-    # deleteDuplicate()
-    # cleanProbePoints()
-
-    # slopeInfo = '0.00/-0.090|110.17/0.062|210.24/0.0746'
-    # distance = 220.00
-    # print(getNodeSlope(distance, slopeInfo))
-
-    # visualizeMatched(links)
+    link_data = load('link data')
+    plotLinkData(link_data)
+# pre-processing
+    cleanLinkData()
+    cleanProbePoints()
+    deleteDuplicate()
+# load cleaned data
+    links = loadCleanData('links')
+    probes = loadCleanData('probes')
+# method 1: point-wise match
+    point_wise_match(links, probes, 50000, os.path.join(geo_path, 'data', 'PointwiseProbes_nodir1.1_50000.csv'))
+# method 2: HMM
+    calculate_hmm_match(links, probes)
+# calculate slopes and do evaluation
+    calculateSlope(links)
+    evaluateSlope()
+# show results
+    visualizeMatched(links)
     plotDistribution()
